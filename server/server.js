461d4226507943f628b769a86a7c691c8914651f
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-11 13:25:42 -0800

    Create new server design and connect to MongoDB

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const db_uri = process.env.MONGODB_URI;
+mongoose.connect(db_uri);
+
+mongoose.connection.once("open", () => {
+  console.log("DB connection successful");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
