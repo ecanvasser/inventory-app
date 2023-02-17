439de1447277d3fca0b090eacaf950c9e5cd8e78
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-16 20:43:08 -0800

    Create initial useEffect for inventory search bar

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -7,9 +7,11 @@ import { useState, useEffect } from "react";
 const Inventory = () => {
   const [products, setProducts] = useState();
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchProducts, setSearchProducts] = useState([]);
   const [makes, setMakes] = useState();
   const [makeFilter, setMakeFilter] = useState();
   const [categoryFilter, setCategoryFilter] = useState();
+  const [search, setSearch] = useState();
   const [categories, setCategories] = useState();
   const [err, setError] = useState();
 
@@ -79,12 +81,43 @@ const Inventory = () => {
     }
   }, [makeFilter, categoryFilter]);
 
+  // Search bar functionality
+  useEffect(() => {
+    if (filteredProducts.length > 0) {
+      const searchArray = filteredProducts.filter((obj) => {
+        if (obj.name.toLowerCase().includes(search)) {
+          return obj
+        }
+      })
+      if (search === "") {
+        setSearchProducts([]);
+      } else {
+        setSearchProducts(searchArray);
+      }
+    } else if (products) {
+      const searchArray = products.filter((obj) => {
+        if (obj.name.toLowerCase().includes(search)) {
+          return obj
+        }
+      })
+      if (search === "") {
+        setSearchProducts([]);
+      } else {
+        setSearchProducts(searchArray);
+      }
+    }
+  }, [search])
+
   const selectMakeFilter = (e) => {
     setMakeFilter(e.target.value);
   };
 
   const selectCategoryFilter = (e) => {
     setCategoryFilter(e.target.value);
+  };
+
+  const searchValue = (e) => {
+    setSearch(e.target.value);
   };
 
   if (err) {
@@ -103,6 +136,40 @@ const Inventory = () => {
     </div>;
   }
 
+  if (searchProducts.length > 0) {
+    return (
+      <div
+        id="inventory-container"
+        className="grid grid-cols-[0.4fr_1.6fr] h-screen"
+      >
+        <Navbar />
+        <div
+          id="products"
+          className="py-16 pl-24 animate__animated animate__slideInDown"
+        >
+          <div className="text-4xl font-extrabold">Inventory</div>
+          <InventoryFilters
+            makes={makes}
+            categories={categories}
+            search={search}
+            handleMakeFilter={selectMakeFilter}
+            handleCategoryFilter={selectCategoryFilter}
+            handleSearch={searchValue}
+          />
+          <div id="product-filters" className="flex"></div>
+          <div
+            id="product-rows"
+            className="mt-16 w-11/12 grid grid-cols-1 gap-4"
+          >
+            {searchProducts.map((obj, i) => {
+              return <InventoryTile data={obj} key={obj._id} />;
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   if (filteredProducts.length > 0) {
     return (
       <div
@@ -118,8 +185,10 @@ const Inventory = () => {
           <InventoryFilters
             makes={makes}
             categories={categories}
+            search={search}
             handleMakeFilter={selectMakeFilter}
             handleCategoryFilter={selectCategoryFilter}
+            handleSearch={searchValue}
           />
           <div id="product-filters" className="flex"></div>
           <div
@@ -150,6 +219,7 @@ const Inventory = () => {
           <InventoryFilters
             makes={makes}
             categories={categories}
+            handleSearch={searchValue}
             handleMakeFilter={selectMakeFilter}
             handleCategoryFilter={selectCategoryFilter}
           />
--- client/src/components/InventoryFilters.js
+++ client/src/components/InventoryFilters.js
@@ -1,10 +1,18 @@
-const InventoryFilters = ({ makes, categories, handleMakeFilter, handleCategoryFilter }) => {
+import { AiOutlineSearch } from "react-icons/ai";
+import { IconContext } from "react-icons";
+
+const InventoryFilters = ({
+  makes,
+  categories,
+  search,
+  handleSearch,
+  handleMakeFilter,
+  handleCategoryFilter,
+}) => {
   return (
-    <div id="filters-container" className="flex gap-8 mt-10">
+    <div id="filters-container" className="flex gap-8 mt-10 items-center">
       <div id="makes-filter">
-        <label className="font-bold text-lg mr-2">
-          Vehicle Make:
-        </label>
+        <label className="font-bold text-lg mr-2">Vehicle Make:</label>
         <select id="makes" className="border p-1" onChange={handleMakeFilter}>
           <option>-</option>
           {makes.map((make, i) => {
@@ -13,15 +21,27 @@ const InventoryFilters = ({ makes, categories, handleMakeFilter, handleCategoryF
         </select>
       </div>
       <div id="category-filter">
-        <label className="font-bold text-lg mr-2">
-          Category:
-        </label>
-        <select id="categories" className="border p-1 pr-10" onChange={handleCategoryFilter}>
+        <label className="font-bold text-lg mr-2">Category:</label>
+        <select
+          id="categories"
+          className="border p-1 pr-10"
+          onChange={handleCategoryFilter}
+        >
           <option>-</option>
           {categories.map((obj, i) => {
             return <option key={i}>{obj.name}</option>;
           })}
         </select>
+      </div>
+      <div id="search-bar" className="flex items-center gap-2 ml-5">
+        <IconContext.Provider value={{ size: "1.8em" }}>
+          <AiOutlineSearch />
+        </IconContext.Provider>
+        <input
+          className="p-1 border rounded"
+          placeholder="Product Name"
+          onChange={handleSearch}
+        />
       </div>
     </div>
   );
