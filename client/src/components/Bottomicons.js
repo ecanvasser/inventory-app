c5b582878ec2e916e9f058f90006d0d641432312
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-21 14:43:06 -0800

    Add links to Bottomicons component

--- client/src/components/Bottomicons.js
+++ client/src/components/Bottomicons.js
@@ -1,20 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Bottomicons = () => {
   return (
     <>
-      <div
-        id="left-iconbox"
-        className="bg-[#FFFACD] flex flex-col items-center text-4xl font-bold px-10 py-16 rounded-2xl"
-      >
-        Add New Product
-      </div>
-      <div
-        id="right-iconbox"
-        className="bg-[#E0FFFF] flex flex-col items-center text-4xl font-bold px-10 py-16 rounded-2xl"
-      >
-        Add New Model
-      </div>
+      <Link to="/inventory/new">
+        <div
+          id="left-iconbox"
+          className="bg-[#FFFACD] flex flex-col items-center text-4xl font-bold px-10 py-16 rounded-2xl"
+        >
+          Add New Product
+        </div>
+      </Link>
+      <Link to="/vehicle/new">
+        <div
+          id="right-iconbox"
+          className="bg-[#E0FFFF] flex flex-col items-center text-4xl font-bold px-10 py-16 rounded-2xl"
+        >
+          Add New Model
+        </div>
+      </Link>
     </>
   );
 };
 
-export default Bottomicons
+export default Bottomicons;
