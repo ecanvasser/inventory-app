c9e97f58d12ed30634f00781cdc33661f105730d
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-22 02:02:06 -0800

    Alter VehicleModel layout for mobile view

--- client/src/components/VehicleModels.js
+++ client/src/components/VehicleModels.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import NavbarLinks from "./NavbarLinks";
 
 const VehicleModels = () => {
   const [models, setModels] = useState();
   const [err, setErr] = useState();
+  const [showLinks, setShowLinks] = useState(false);
 
   useEffect(() => {
     const fetchModels = async () => {
@@ -39,55 +41,71 @@ const VehicleModels = () => {
     return (
       <div
         id="models-container"
-        className="grid grid-cols-[0.4fr_1.6fr] h-screen"
+        className="grid grid-rows-[0.3fr_1.7fr] h-screen"
       >
-        <Navbar />
-        <div
-          id="models-body"
-          className="w-10/12 py-16 pl-32 animate__animated animate__slideInRight"
-        >
-          <div id="section-title" className="text-4xl font-extrabold">
-            Vehicle Models
+        <Navbar
+          handleNav={() => {
+            setShowLinks(!showLinks);
+          }}
+        />
+        {showLinks ? (
+          <div className="flex flex-col gap-10 items-center animate__animated animate__fadeInRight">
+            <NavbarLinks />
           </div>
-          <div id="model-tiles" className="mt-16 flex flex-col gap-16">
-            <div id="audi-models" className="flex flex-col">
-              <div id="audi-title" className="font-bold text-2xl mb-4">
-                Audi Models
+        ) : (
+          <div
+            id="models-body"
+            className="flex flex-col items-center animate__animated animate__slideInLeft"
+          >
+            <div id="section-title" className="text-4xl font-extrabold">
+              Vehicle Models
+            </div>
+            <div id="model-tiles" className="mt-16 flex flex-col gap-16">
+              <div id="audi-models" className="flex flex-col items-center">
+                <div id="audi-title" className="font-bold text-2xl mb-4">
+                  Audi Models
+                </div>
+                <div id="audimodels-grid" className="flex flex-wrap gap-5">
+                  {models.map((obj) => {
+                    if (obj.make === "Audi") {
+                      return (
+                        <Link key={obj._id} to={`/vehicle/${obj._id}`}>
+                          <div
+                            id="model-tile"
+                            className="bg-[#E0FFFF] p-2 border rounded-xl"
+                          >
+                            {obj.make} {obj.model}
+                          </div>
+                        </Link>
+                      );
+                    }
+                  })}
+                </div>
               </div>
-              <div id="audimodels-grid" className="flex flex-wrap gap-5">
-                {models.map((obj) => {
-                  if (obj.make === "Audi") {
-                    return (
-                      <Link key={obj._id} to={`/vehicle/${obj._id}`}>
-                        <div id="model-tile" className="bg-[#E0FFFF] p-2 border rounded-xl">
-                          {obj.make} {obj.model}
-                        </div>
-                      </Link>
-                    );
-                  }
-                })}
+              <div id="vw-models" className="flex flex-col items-center">
+                <div id="vw-title" className="font-bold text-2xl mb-4">
+                  Volkswagen Models
+                </div>
+                <div id="vwmodels-grid" className="flex flex-wrap gap-5">
+                  {models.map((obj) => {
+                    if (obj.make === "Volkswagen") {
+                      return (
+                        <Link key={obj._id} to={`/vehicle/${obj._id}`}>
+                          <div
+                            id="model-tile"
+                            className="bg-[#E0FFFF] p-2 border rounded-xl"
+                          >
+                            {obj.make} {obj.model}
+                          </div>
+                        </Link>
+                      );
+                    }
+                  })}
+                </div>
               </div>
             </div>
-            <div id="vw-models" className="flex flex-col">
-              <div id="vw-title" className="font-bold text-2xl mb-4">
-                Volkswagen Models
-              </div>
-              <div id="vwmodels-grid" className="flex flex-wrap gap-5">
-              {models.map((obj) => {
-                  if (obj.make === "Volkswagen") {
-                    return (
-                      <Link key={obj._id} to={`/vehicle/${obj._id}`}>
-                        <div id="model-tile" className="bg-[#E0FFFF] p-2 border rounded-xl">
-                          {obj.make} {obj.model}
-                        </div>
-                      </Link>
-                    );
-                  }
-                })}
-              </div>
-            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
