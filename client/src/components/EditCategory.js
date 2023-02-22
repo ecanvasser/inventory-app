b3262cf80172ddca4b1c1f93763c6fd3787530de
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-22 02:42:40 -0800

    Update EditCategory layout for mobile

--- client/src/components/EditCategory.js
+++ client/src/components/EditCategory.js
@@ -2,12 +2,14 @@ import Navbar from "./Navbar";
 import CategoryMessage from "./CategoryMessage";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import NavbarLinks from "./NavbarLinks";
 
 const EditCategory = () => {
   const [category, setCategory] = useState();
   const [relatedProducts, setRelatedProducts] = useState();
   const [showMessage, setShowMessage] = useState(false);
   const [error, setError] = useState();
+  const [showLinks, setShowLinks] = useState(false);
   const params = useParams();
   const navigate = useNavigate();
 
@@ -101,46 +103,66 @@ const EditCategory = () => {
     return (
       <div
         id="editcategory-container"
-        className="grid grid-cols-[0.4fr_1.6fr] h-screen"
+        className="grid grid-rows-[0.3fr_1.7fr] h-screen"
       >
-        <Navbar />
-        <div
-          id="editcategory-body"
-          className="py-16 pl-24 animate__animated animate__slideInDown"
-        >
-          <div id="editcategory-title" className="text-4xl font-extrabold">
-            Edit Category
+        <Navbar
+          handleNav={() => {
+            setShowLinks(!showLinks);
+          }}
+        />
+        {showLinks ? (
+          <div className="flex flex-col gap-10 items-center animate__animated animate__fadeInRight">
+            <NavbarLinks />
           </div>
-          <form
-            id="editcategory"
-            onSubmit={formUpdate}
-            className="mt-10 flex flex-col gap-6"
+        ) : (
+          <div
+            id="editcategory-body"
+            className="flex flex-col items-center animate__animated animate__slideInLeft"
           >
-            <label className="flex gap-2">
-              <div className="text-lg font-bold">Name:</div>
-              <input
-                type="text"
-                onChange={(e) => {
-                  setCategory(e.target.value);
-                }}
-                value={category}
-                required
-                className="py-1 pl-2 border rounded"
-              />
-            </label>
-            <div id="buttons" className="flex gap-4">
-              <input
-                type="submit"
-                value="Update Category"
-                className="w-max px-2 py-1 border rounded bg-[#ccffcc]"
-              />
-              <button onClick={formDelete} className="border bg-[#f4a4a4] rounded px-2 py-1">
-                Delete
-              </button>
+            <div id="editcategory-title" className="text-4xl font-extrabold">
+              Edit Category
             </div>
-          </form>
-          {showMessage ? <CategoryMessage products={relatedProducts} closeMessage={() => {setShowMessage(false)}} /> : null}
-        </div>
+            <form
+              id="editcategory"
+              onSubmit={formUpdate}
+              className="mt-10 flex flex-col items-center gap-8"
+            >
+              <label className="flex gap-2">
+                <div className="text-lg font-bold">Name:</div>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}
+                  value={category}
+                  required
+                  className="py-1 pl-2 border rounded"
+                />
+              </label>
+              <div id="buttons" className="flex gap-4">
+                <input
+                  type="submit"
+                  value="Update Category"
+                  className="w-max px-2 py-1 border rounded bg-[#ccffcc]"
+                />
+                <button
+                  onClick={formDelete}
+                  className="border bg-[#f4a4a4] rounded px-2 py-1"
+                >
+                  Delete
+                </button>
+              </div>
+            </form>
+            {showMessage ? (
+              <CategoryMessage
+                products={relatedProducts}
+                closeMessage={() => {
+                  setShowMessage(false);
+                }}
+              />
+            ) : null}
+          </div>
+        )}
       </div>
     );
   }
