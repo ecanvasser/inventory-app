aed7a04b13e3dc46591b02b7e53f25fee9660427
ecanvasser <eddiecanvassere9@gmail.com>
2023-02-22 02:32:10 -0800

    Update NewCategory component for mobile

--- client/src/components/NewCategory.js
+++ client/src/components/NewCategory.js
@@ -1,63 +1,82 @@
 import Navbar from "./Navbar";
 import { useState } from "react";
+import NavbarLinks from "./NavbarLinks";
 
 const NewCategory = () => {
   const [newcategory, setNewCategory] = useState();
   const [apiresponse, setApiResponse] = useState();
   const [error, setError] = useState();
+  const [showLinks, setShowLinks] = useState(false);
 
   const handleChange = (e) => {
     setNewCategory(e.target.value);
-  }
-  
+  };
+
   const handleSubmit = () => {
     const requestOptions = {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({name: newcategory})
-    }
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: newcategory }),
+    };
     const postCategory = async () => {
-        try {
-            const response = await fetch("http://localhost:8000/product-categories/new", requestOptions);
-            const data = await response.json();
-            console.log(data);
-            setApiResponse(data);
-        } catch (err) {
-            setError(err);
-        }
-    }
+      try {
+        const response = await fetch(
+          "http://localhost:8000/product-categories/new",
+          requestOptions
+        );
+        const data = await response.json();
+        console.log(data);
+        setApiResponse(data);
+      } catch (err) {
+        setError(err);
+      }
+    };
     postCategory();
-  }
-  
-    return (
+  };
+
+  return (
     <div
       id="newcategory-container"
-      className="grid grid-cols-[0.4fr_1.6fr] h-screen"
+      className="grid grid-rows-[0.3fr_1.7fr] h-screen"
     >
-      <Navbar />
-      <div
-        id="newcategory-body"
-        className="py-16 pl-24 animate__animated animate__slideInDown"
-      >
-        <div id="nc-title" className="text-4xl font-extrabold">
-          Add New Category
+      <Navbar
+        handleNav={() => {
+          setShowLinks(!showLinks);
+        }}
+      />
+      {showLinks ? (
+        <div className="flex flex-col gap-10 items-center animate__animated animate__fadeInRight">
+          <NavbarLinks />
         </div>
-        <form id="nc-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-10">
-          <label className="flex gap-2 font-bold text-xl">
-            Category Name:
+      ) : (
+        <div
+          id="newcategory-body"
+          className="flex flex-col items-center animate__animated animate__slideInLeft"
+        >
+          <div id="nc-title" className="text-4xl font-extrabold">
+            Add New Category
+          </div>
+          <form
+            id="nc-form"
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center gap-8 mt-10"
+          >
+            <label className="flex gap-2 font-bold text-xl">
+              Category Name:
+              <input
+                type="text"
+                className="border rounded pl-2 font-medium"
+                placeholder="Category"
+                onChange={handleChange}
+              />
+            </label>
             <input
-              type="text"
-              className="border rounded font-medium"
-              placeholder="Category"
-              onChange={handleChange}
+              type="submit"
+              className="w-max border py-1 px-3 rounded bg-[#ccffcc]"
             />
-          </label>
-          <input
-            type="submit"
-            className="w-max border py-1 px-3 rounded bg-[#ccffcc]"
-          />
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
